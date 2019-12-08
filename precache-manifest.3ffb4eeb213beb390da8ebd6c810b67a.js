7d3f43562051eaa6dba4fb6d67b46e6b04c4af26
josue rojas <josuerojas.rojas@gmail.com>
2019-12-07 18:14:21 -0600

    Updates

---
+++ precache-manifest.3ffb4eeb213beb390da8ebd6c810b67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3380489730dbd197158011608d8b8b",
+    "url": "/idk/index.html"
+  },
+  {
+    "revision": "1b31f0c2d388f2b19a9b",
+    "url": "/idk/static/css/main.959e763e.chunk.css"
+  },
+  {
+    "revision": "89f0e6d1e9f7b14a3aae",
+    "url": "/idk/static/js/2.1267f246.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/idk/static/js/2.1267f246.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b31f0c2d388f2b19a9b",
+    "url": "/idk/static/js/main.0e4114ae.chunk.js"
+  },
+  {
+    "revision": "d9ca4e59c91cd9a5b902",
+    "url": "/idk/static/js/runtime-main.b0769aa7.js"
+  }
+]);
